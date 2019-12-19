46ab3c784572e738b0a757b5e11cf6b0e1ff3f06
Aris Ripandi <ar.is@outlook.com>
2019-12-19 11:42:07 +0700

    add meta description

--- components/layout.js
+++ components/layout.js
@@ -136,19 +136,19 @@ export default function Layout({ meta, children }) {
               <Link href="/docs">
                 <a className="block flex items-center mr-6 px-2 py-3 font-bold uppercase text-secondary hover:text-teal-400">Docs</a>
               </Link>
-              <a className="block flex items-center hover:text-teal-400 mr-5" href="//github.com/varletdev" target="_blank">
+              <a className="block flex items-center hover:text-teal-400 mr-5" href="//github.com/varletdev" target="_blank" rel="noopener">
                 <svg className="fill-current w-6 h-6" viewBox="0 0 20 20">
                   <title>GitHub</title>
                   <path d="M10 0a10 10 0 0 0-3.16 19.49c.5.1.68-.22.68-.48l-.01-1.7c-2.78.6-3.37-1.34-3.37-1.34-.46-1.16-1.11-1.47-1.11-1.47-.9-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.9 1.52 2.34 1.08 2.91.83.1-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.94 0-1.1.39-1.99 1.03-2.69a3.6 3.6 0 0 1 .1-2.64s.84-.27 2.75 1.02a9.58 9.58 0 0 1 5 0c1.91-1.3 2.75-1.02 2.75-1.02.55 1.37.2 2.4.1 2.64.64.7 1.03 1.6 1.03 2.69 0 3.84-2.34 4.68-4.57 4.93.36.31.68.92.68 1.85l-.01 2.75c0 .26.18.58.69.48A10 10 0 0 0 10 0" />
                 </svg>
               </a>
-              <a className="block flex items-center hover:text-teal-400 mr-4" href="//twitter.com/riipandi" target="_blank">
+              <a className="block flex items-center hover:text-teal-400 mr-4" href="//twitter.com/riipandi" target="_blank" rel="noopener">
                 <svg className="fill-current w-6 h-6" viewBox="0 0 20 20">
                   <title>Twitter</title>
                   <path d="M6.29 18.25c7.55 0 11.67-6.25 11.67-11.67v-.53c.8-.59 1.49-1.3 2.04-2.13-.75.33-1.54.55-2.36.65a4.12 4.12 0 0 0 1.8-2.27c-.8.48-1.68.81-2.6 1a4.1 4.1 0 0 0-7 3.74 11.65 11.65 0 0 1-8.45-4.3 4.1 4.1 0 0 0 1.27 5.49C2.01 8.2 1.37 8.03.8 7.7v.05a4.1 4.1 0 0 0 3.3 4.03 4.1 4.1 0 0 1-1.86.07 4.1 4.1 0 0 0 3.83 2.85A8.23 8.23 0 0 1 0 16.4a11.62 11.62 0 0 0 6.29 1.84" />
                 </svg>
               </a>
-              <a className="block flex items-center mr-8 hover:text-teal-400" href="//spectrum.chat/varlet" target="_blank">
+              <a className="block flex items-center mr-8 hover:text-teal-400" href="//spectrum.chat/varlet" target="_blank" rel="noopener">
                 <svg className="fill-current w-6 h-6" viewBox="0 0 26 26">
                   <title>Join our community</title>
                   <path d="M6 14.5C6 15.3284 6.67157 16 7.5 16H9C12.866 16 16 19.134 16 23V24.5C16 25.3284 16.6716 26 17.5 26H24.5C25.3284 26 26 25.3284 26 24.5V23C26 13.6111 18.3889 6 9 6H7.5C6.67157 6 6 6.67157 6 7.5V14.5Z"/>
@@ -156,7 +156,7 @@ export default function Layout({ meta, children }) {
               </a>
               <a
                 className="block flex items-center bg-teal-600 hover:bg-teal-700 text-white hover:text-white font-semibold py-3 px-8 rounded"
-                href="//github.com/varletdev/varlet/releases" target="_blank">
+                href="//github.com/varletdev/varlet/releases" target="_blank" rel="noopener">
                 Download
               </a>
             </div>
@@ -174,7 +174,7 @@ export default function Layout({ meta, children }) {
                 </p>
                 <p className="mt-6 text-sm leading-relaxed max-w-md pr-2 italic">
                   The Varlet package doesn't include any database engine,<br/> but you can try{' '}
-                  <a className="font-bold text-teal-300 hover:text-teal-400 border-b border-dashed" href="//github.com/varletdev/addons" target="_blank">our add-on</a>.
+                  <a className="font-bold text-teal-300 hover:text-teal-400 border-b border-dashed" href="//github.com/varletdev/addons" target="_blank" rel="noopener">our add-on</a>.
                 </p>
               </div>
               <div className="w-full lg:w-1/2 lg:pl-4 mt-8 lg:mt-0 mb-12 hidden lg:flex">
@@ -245,7 +245,7 @@ export default function Layout({ meta, children }) {
         <div className="container mx-auto py-6 text-white text-center">
         <p>
             Copyright &copy; 2019 {' - '}
-            <span className="hidden md:inline">Varlet is{' '}</span>a project by <a href="//arisio.us" className="inline font-medium border-b border-dashed" target="_blank">Aris Ripandi</a>
+            <span className="hidden md:inline">Varlet is{' '}</span>a project by <a href="//arisio.us" className="inline font-medium border-b border-dashed" target="_blank" rel="noopener">Aris Ripandi</a>
         </p>
         </div>
     </footer>
